bf0e6125dc5b57b4036c8c1156feee4e9aac476f
Softec Softwares <frankkarlseng@gmail.com>
2025-07-11 18:29:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Prices from "./components/Prices";
-import CallToAction from "./components/CallToAction";
 import FAQSection from "./components/FAQSection";
 import Footer from "./components/Footer";
 
