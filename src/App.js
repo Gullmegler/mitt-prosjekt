41099731cc59fc6a129559bb1d570387913a01fd
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 10:43:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Benefits from "./components/Benefits";
-import Prices from "./components/Prices";
-import CallToAction from "./components/CallToAction";
-import FAQSection from "./components/FAQSection";
-import Footer from "./components/Footer";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';  // Import SignUp component
+import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+import Prices from './components/Prices';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="font-sans">
-      <Navbar />
-      <Hero />
-      <Benefits />
-      <Prices />
-      <CallToAction />
-      <FAQSection />
-      <Footer />
-    </div>
+    <Router>
+      <div className="font-sans">
+        <Navbar />
+        <Switch>
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />  {/* Add route for SignUp */}
+          {/* Add other routes here */}
+        </Switch>
+        <Hero />
+        <Benefits />
+        <Prices />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
