3f7f3c559081985db05807b874d43cb22a586507
Softec Softwares <frankkarlseng@gmail.com>
2025-07-13 00:26:37 +0100

    Update SofaIcon.jsx

--- src/components/icons/SofaIcon.jsx
+++ src/components/icons/SofaIcon.jsx
@@ -1,1 +1,16 @@
-
+export default function SofaIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      className="w-12 h-12"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect x="2" y="10" width="20" height="8" rx="2" strokeWidth="2"></rect>
+      <path d="M4 18v2" strokeWidth="2"></path>
+      <path d="M20 18v2" strokeWidth="2"></path>
+    </svg>
+  );
+}
