b5d5573a3b03db932f1b32363a5f490a43614dc4
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 15:41:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
-import Hero from './components/Hero';
-import Benefits from './components/Benefits';
-import Prices from './components/Prices';
-import FAQSection from './components/FAQSection';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Prices from "./components/Prices";
+import FAQSection from "./components/FAQSection";
+import SignUp from "./components/SignUp";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className="font-sans">
-        <Navbar />
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <section id="benefits">
-                  <Benefits />
-                </section>
-                <section id="prices">
-                  <Prices />
-                </section>
-                <section id="faq">
-                  <FAQSection />
-                </section>
-                <Footer />
-              </>
-            }
-          />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <section id="benefits">
+                <Benefits />
+              </section>
+              <section id="prices">
+                <Prices />
+              </section>
+              <section id="faq">
+                <FAQSection />
+              </section>
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </Router>
   );
 }
-
-export default App;
