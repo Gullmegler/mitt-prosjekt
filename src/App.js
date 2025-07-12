dd17a1210d0346dda6b672b7ff329a78b7264908
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 15:58:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Prices from "./components/Prices";
