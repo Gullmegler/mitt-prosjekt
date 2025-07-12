dacfa67eba664dabad4eea6cfac946731561a4ba
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 23:43:06 +0100

    Create Sofa.jsx

---
+++ src/components/Sofa.jsx
@@ -0,0 +1,9 @@
+// Sofa.jsx
+export default function Sofa(props) {
+  return (
+    <svg {...props} fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
+      <path d="M3 10v6h18v-6a3 3 0 00-3-3H6a3 3 0 00-3 3z" />
+      <path d="M3 16v2h18v-2" />
+    </svg>
+  );
+}
