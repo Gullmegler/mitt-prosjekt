884916e36b3f58b10b2b1999b9fff879b84ce98b
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 23:57:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import AiSurvey from "./components/AiSurvey";
 import Prices from "./components/Prices";
 import FAQSection from "./components/FAQSection";
-import Footer from "./components/Footer";
-import SignUp from "./components/SignUp";
 import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <section id="benefits">
-                <Benefits />
-              </section>
-              <section id="prices">
-                <Prices />
-              </section>
-              <CallToAction /> {/* Legges her, før FAQ */}
-              <section id="faq">
-                <FAQSection />
-              </section>
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+      <Hero />
+      <Benefits />
+      <AiSurvey />
+      <Prices />
+      <CallToAction />
+      <FAQSection /> {/* ✅ Nå rett før Footer */}
+      <Footer />
+    </>
   );
 }
+
+export default App;
