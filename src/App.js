6cfead35057c934b0987e9050b556789f7599cbc
Softec Softwares <frankkarlseng@gmail.com>
2025-07-12 10:57:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';  // Import SignUp component
+import SignUp from './components/SignUp';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Prices from './components/Prices';
+import FAQSection from './components/FAQSection';
 import Footer from './components/Footer';
+import Contact from './components/Contact';  // Legg til en kontaktseksjon
 
 function App() {
   return (
@@ -15,12 +17,28 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/signin" component={SignIn} />
-          <Route path="/signup" component={SignUp} />  {/* Add route for SignUp */}
-          {/* Add other routes here */}
+          <Route path="/signup" component={SignUp} />
+          {/* Andre ruter */}
         </Switch>
+        
         <Hero />
-        <Benefits />
-        <Prices />
+        
+        <section id="benefits">
+          <Benefits />
+        </section>
+        
+        <section id="prices">
+          <Prices />
+        </section>
+
+        <section id="faq">
+          <FAQSection />
+        </section>
+
+        <section id="contact">
+          <Contact />
+        </section>
+
         <Footer />
       </div>
     </Router>
