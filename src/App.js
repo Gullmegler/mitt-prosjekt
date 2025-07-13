1d0dc6cabcf335c55516613e43912118cfcdf1e8
Softec Softwares <frankkarlseng@gmail.com>
2025-07-13 13:09:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import AiSurvey from "./components/AiSurvey";
 import Prices from "./components/Prices";
 import FAQSection from "./components/FAQSection";
-import CallToAction from "./components/CallToAction";
 import Footer from "./components/Footer";
 
 function App() {
@@ -16,7 +14,6 @@ function App() {
       <Benefits />
       <AiSurvey />
       <Prices />
-      <CallToAction />
       <FAQSection />
       <Footer />
     </>
