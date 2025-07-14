ffefaa8d033d66148d67b8761f93d905398c924d
Softec Softwares <frankkarlseng@gmail.com>
2025-07-14 22:33:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,21 +10,10 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="bg-githubbg text-white">
+    <div className="bg-github text-white">
       <Navbar />
       <Hero />
-      <section id="benefits" className="flex flex-col md:flex-row items-center justify-between px-8 py-16">
-        <div className="md:w-1/2">
-          <Benefits />
-        </div>
-        <div className="md:w-1/2 flex justify-center mt-8 md:mt-0">
-          <img
-            src="/survey-agent.png"
-            alt="Survey Agent"
-            className="max-w-full h-auto rounded"
-          />
-        </div>
-      </section>
+      <Benefits />
       <AiSurvey />
       <Prices />
       <FAQ />
