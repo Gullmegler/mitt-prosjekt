cbf9364929c04deff72408124ed3696f2105dc9c
Softec Softwares <frankkarlseng@gmail.com>
2025-07-14 21:49:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,29 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-background text-text">
       <Navbar />
-      <Hero />
-      <Benefits />
-      <AiSurvey />
-      <Prices />
-      <FAQ />
-      <CallToAction />
+
+      <main>
+        <Hero />
+        <section id="benefits" className="bg-background text-text">
+          <Benefits />
+        </section>
+        <section id="ai-survey" className="bg-background text-text">
+          <AiSurvey />
+        </section>
+        <section id="prices" className="bg-background text-text">
+          <Prices />
+        </section>
+        <section id="faq" className="bg-background text-text">
+          <FAQ />
+        </section>
+        <section id="cta" className="bg-background text-text">
+          <CallToAction />
+        </section>
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
