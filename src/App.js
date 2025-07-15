610f4ef841957f34f22c58903e9f394cd05f1b8b
Softec Softwares <frankkarlseng@gmail.com>
2025-07-15 10:39:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Prices from "./components/Prices";
 import FAQSection from "./components/FAQSection";
 import CallToAction from "./components/CallToAction";
 import Footer from "./components/Footer";
-import Login from "./components/Login";
+import Login from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Navbar from "./components/Navbar";
 
