6af76ec93e599b7fe3e6e22a1769937ec6c816b4
Softec Softwares <frankkarlseng@gmail.com>
2025-07-15 10:18:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Prices from "./components/Prices";
-import FAQ from "./components/FAQSection";
+import FAQSection from "./components/FAQSection";
 import CallToAction from "./components/CallToAction";
 import Footer from "./components/Footer";
-import Privacy from "./components/Privacy";
-import Terms from "./components/Terms";
+import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import LogIn from "./components/LogIn";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="bg-[#111444] text-white">
@@ -26,20 +23,18 @@ function App() {
                 <Hero />
                 <Benefits />
                 <Prices />
-                <FAQ />
+                <FAQSection />
                 <CallToAction />
               </>
             }
           />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<LogIn />} />
         </Routes>
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
