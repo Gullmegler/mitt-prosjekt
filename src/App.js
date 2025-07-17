6517b9cc8ef117f3736c8d2ab04459a4231a504e
Softec Softwares <frankkarlseng@gmail.com>
2025-07-17 10:03:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Hero from "./components/Hero";
-import Benefits from "./components/Benefits";
-import AiSurvey from "./components/AiSurvey";
-import Prices from "./components/Prices";
-import FAQSection from "./components/FAQSection";
-import CallToAction from "./components/CallToAction";
-import Footer from "./components/Footer";
-import LogIn from "./components/LogIn";
-import SignUp from "./components/SignUp";
-import Privacy from "./components/Privacy";
-import Terms from "./components/Terms";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
+import Terms from "./components/Terms";
+import Privacy from "./components/Privacy";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="bg-[#111144] text-white">
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <Benefits />
-              <AiSurvey />
-              <Prices />
-              <FAQSection />
-              <CallToAction />
-            </>
-          }
-        />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
