c87692681b49c8b7870e7f620043e61670e7a8a7
Softec Softwares <frankkarlseng@gmail.com>
2025-07-18 10:00:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Terms from "./components/Terms";
 import Privacy from "./components/Privacy";
-import Footer from "./components/Footer";
+import Benefits from "./components/Benefits";
+import FAQSection from "./components/FAQSection";
+import Prices from "./components/Prices";
+import CallToAction from "./components/CallToAction";
+import AiSurvey from "./components/AiSurvey";
+import Users from "./components/Users";
 
 const App = () => {
   return (
@@ -19,6 +26,12 @@ const App = () => {
           <Route path="/login" element={<LogIn />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
+          <Route path="/benefits" element={<Benefits />} />
+          <Route path="/faq" element={<FAQSection />} />
+          <Route path="/prices" element={<Prices />} />
+          <Route path="/cta" element={<CallToAction />} />
+          <Route path="/survey" element={<AiSurvey />} />
+          <Route path="/users" element={<Users />} />
         </Routes>
       </main>
       <Footer />
